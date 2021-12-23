d4f6397a4c83554659cdb7e5214d0b616c3629cc
Yoav Ben Sinai <yoavbs2004@gmail.com>
2021-12-23 14:58:41 +0200

    finally the server works, sending still suck but...

--- FT.py
+++ FT.py
@@ -5,14 +5,13 @@ import socket
 app = Flask(__name__)
 ToSend = []
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 8080))
+client.connect(('127.0.0.1', 5053))
 
 @app.route('/success')
 def success():
     while len(ToSend) != 0:
         client.send(ToSend.pop(0).encode())
-    print(ToSend)
-    return 'Thanks for answering the survey%'
+    return 'Thanks for answering the survey'
 
 
 @app.route('/Que', methods=['POST', 'GET'])
--- server.py
+++ server.py
@@ -1,13 +1,48 @@
-import socket
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('0.0.0.0', 8080))
-serv.listen(5)
-while True:
-    conn, addr = serv.accept()
-    print("connection" )
-    from_client = ''
-    while True:
-        data = conn.recv(1024)
-        from_client += data
-        print(from_client)
-    conn.close()
+from socket import *
+import threading
+
+SERVER_ADDRESS = ("0.0.0.0", 5053)
+client_list = []
+
+
+class ThreadClass(threading.Thread):
+    def __init__(self, val):
+        threading.Thread.__init__(self)
+        self.val = val
+
+    def getName(self):
+        return self.val
+
+    def run(self):
+        while True:
+            m = (self.val.recv(1024)).decode()
+            print(m)
+            if not m:
+                break
+            for i in client_list:
+                i.send(("server says " + m).encode())
+
+
+
+class Server(ThreadClass):
+    def __init__(self):
+        print("init")
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind(SERVER_ADDRESS)
+        self.socket.listen(3)  # The serversock is listening to requests
+
+    def run_server(self):
+        print("run_server")
+        while True:
+            global clientList
+            print("Waiting for connection..")
+            clientsock, addr = self.socket.accept()
+            print("Connected from:", addr)
+            client_list.append(clientsock)
+            th1 = ThreadClass(clientsock)
+            th1.start()
+
+
+
+s = Server()
+s.run_server()
